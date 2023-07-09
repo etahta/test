674fd47e62b4f1095e326d66cf28b7108f24977e
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2023-07-09 13:32:17 +0300

    Update 4_firmware.sh

--- 4_firmware.sh
+++ 4_firmware.sh
@@ -6,33 +6,32 @@
       #firmware-intel-sound zd1211-firmware firmware-sof-signed  firmware-siano firmware-myricom firmware-netxen firmware-qlogic firmware-cavium firmware-atheros \
 
 chroot chroot apt-get install \
-      firmware-amd-graphics \
-      firmware-intel-sound \
-      firmware-sof-signed \
-      zd1211-firmware \
-      firmware-netxen \
-      firmware-qlogic \    
-      firmware-iwlwifi \
-      firmware-linux \
-      firmware-linux-free \
-      firmware-linux-nonfree \
-      firmware-misc-nonfree \
-      firmware-ralink \
-      firmware-realtek \
-      firmware-atheros \
-      firmware-samsung \
-      firmware-ti-connectivity -y
+firmware-amd-graphics \
+firmware-intel-sound \
+firmware-sof-signed \
+zd1211-firmware \
+firmware-netxen \
+firmware-qlogic \
+firmware-iwlwifi \
+firmware-linux \
+firmware-linux-free \
+firmware-linux-nonfree \
+firmware-misc-nonfree \
+firmware-ralink \
+firmware-realtek \
+firmware-atheros \
+firmware-samsung \
+firmware-ti-connectivity -y
 
      # firmware-siano \
      # bluez-firmware \     
      # firmware-myricom \
-
      # firmware-cavium \
      # firmware-bnx2 \
      # firmware-bnx2x \
      # firmware-brcm80211 \
      #firmware-libertas \
-     #firmware-atheros \
+
 chroot chroot apt --fix-broken install -y
 chroot chroot apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
 
